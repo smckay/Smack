691d4e95d9459bad76aa3a5e7080048dc20a5865
Sam McKay <sam.mckay95@gmail.com>
2018-08-02 15:35:58 -0400

    login UI configuration

---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Sam McKay on 8/2/18.
+//  Copyright © 2018 Sam McKay. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
+
---
+++ Smack/View/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Sam McKay on 8/2/18.
+//  Copyright © 2018 Sam McKay. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
